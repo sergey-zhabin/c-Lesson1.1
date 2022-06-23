ccb8fb9b8882bfd0e96aec4eb3a2d2b11940388f
Sergey <santexnik.86@mail.ru>
2022-06-23 18:45:20 +0300

    Examples005If-Else

---
+++ Exanples005ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МАША!");
+    
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
